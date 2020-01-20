30266fcbdf3c6bc7e879cedd76899b95e0538be7
CamilleLP4 <camille.paudrat.lp4@gmail.com>
2020-01-20 16:58:36 +0100

    7 files modified, 7 added

--- src/app/princess-details/princess-details.component.ts
+++ src/app/princess-details/princess-details.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 export class PrincessDetailsComponent implements OnInit {
 
   @Input() inputPrincess;
+  @Input() iPrincessId;
   @Output() like = new EventEmitter();
 
   constructor() { }
---
+++ src/app/princessList.ts
@@ -0,0 +1,26 @@
+export const princessList = [
+    {
+      name: 'Blanche Neige',
+      year: 1938, imageUrl:'https://www.teteamodeler.com/images/assets/8508bc69207e232f9f94de031d61c85a.jpg',
+      like: 0,
+      script: "Une bonne reine se désole de ne pas avoir d'enfant. Un jour d’hiver, alors qu'elle est assise près d'une fenêtre au cadre d'ébène, elle se pique le doigt en cousant. Quelques gouttes de sang tombent sur la neige.  Ah !  se dit la reine,  si j'avais une petite fille, à la peau blanche comme la neige, aux lèvres rouges comme le sang et aux cheveux noirs comme le bois d’ébène ! ."
+    },
+    {
+      name: 'Cendrillon',
+      year: 1950, imageUrl:'https://www.teteamodeler.com/images/assets/6626a421eda3a7020e74b6d33fce244e.jpg',
+      like: 0,
+      script: "test"
+    },
+    {
+      name: 'Josselin',
+      year: 2019, imageUrl:'https://cdn.discordapp.com/attachments/631565747281199104/656950061712015392/201907311605280471226.jpg',
+      like: 0,
+      script: "Josselion"
+    },
+    {
+      name: 'Jules',
+      year: 2020,
+      imageUrl:'',
+      like: 0
+    }
+  ];
